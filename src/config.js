2bda0c0a54500d33cb97487a36aabb8fe89bab06
AndresLopezAlvarado <andreslopezalvarado@hotmail.com>
2024-02-27 12:27:23 -0500

    se ha actualizado el link de conexion a base de datos

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 4000;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "cualquier clave secreta";
-export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost/merndb";
+export const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://andreslopez:andreslopez@cluster0.3ijfq0d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5000";
